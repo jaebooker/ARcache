b0b47b9e3489982cb75bfa738f1ab5ab97283090
jaebooker <jaesonbooker@gmail.com>
2019-02-27 21:56:30 -0800

    got physics to work

--- ARcache/ViewController.swift
+++ ARcache/ViewController.swift
@@ -16,7 +16,13 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        addCache()
+        //        // Create a new scene
+        //        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        //
+        //        // Set the scene to the view
+        //        sceneView.scene = scene
+        registerGestureRecognizer()
         // Set the view's delegate
         sceneView.delegate = self
         
@@ -24,12 +30,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         let scene = SCNScene()
         sceneView.scene = scene
-        addCache()
-//        // Create a new scene
-//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-//
-//        // Set the scene to the view
-//        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -49,31 +49,62 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {return}
-        let result = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
-        guard let hitResult = result.last else {return}
-        //let hitTransform = hitResult.worldTransform
-        let hitTransform = SCNMatrix4.init(hitResult.worldTransform)
-//        let hitTransform = SCNMatrix4FromGLKMatrix4(hitResult?.worldTransform)
-        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
-        createCache(position: hitVector)
-    }
-    func createCache(position: SCNVector3) {
-        let cacheShape = SCNBox(width: 0.1, height: 0.1, length: 0.2, chamferRadius: 0.1)
-        let cacheNode = SCNNode(geometry: cacheShape)
-        cacheNode.position = position
-        sceneView.scene.rootNode.addChildNode(cacheNode)
-    }
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        guard let touch = touches.first else {return}
+//        let result = sceneView.hitTest(touch.location(in: sceneView), types: [ARHitTestResult.ResultType.featurePoint])
+//        guard let hitResult = result.last else {return}
+//        //let hitTransform = hitResult.worldTransform
+//        let hitTransform = SCNMatrix4.init(hitResult.worldTransform)
+////        let hitTransform = SCNMatrix4FromGLKMatrix4(hitResult?.worldTransform)
+//        let hitVector = SCNVector3Make(hitTransform.m41, hitTransform.m42, hitTransform.m43)
+//        createCache(position: hitVector)
+//    }
+//    func createCache(position: SCNVector3) {
+//        let cacheShape = SCNBox(width: 0.1, height: 0.1, length: 0.2, chamferRadius: 0.1)
+//        let material = SCNMaterial()
+//        material.diffuse.contents = UIImage(named: "texture")
+//        //diffuse (how light renders), contents (appearance of material)
+//        cacheShape.materials = [material]
+//        let cacheNode = SCNNode(geometry: cacheShape)
+//        cacheNode.position = position
+//        sceneView.scene.rootNode.addChildNode(cacheNode)
+//    }
     func registerGestureRecognizer(){
-        let tap = UITapGestureRecognizer(target: self, action: #selector(handletap))
-         
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tap)
+        
     }
     func addCache() {
         guard let cacheScene = SCNScene(named: "art.scnassets/ship.scn") else { return }
         guard let cacheSceneNode = cacheScene.rootNode.childNode(withName: "cache", recursively: false) else { return }
         cacheSceneNode.position = SCNVector3(0, 0.5, -3)
         sceneView.scene.rootNode.addChildNode(cacheSceneNode)
+        print("cacheSceneNode added")
+    }
+    @objc func handleTap(gestureRecognizer: UITapGestureRecognizer){
+        //scene view
+        //access point of view (center point)
+        guard let sceneView = gestureRecognizer.view as? ARSCNView else { return }
+        guard let centerPoint = sceneView.pointOfView else { return }
+        //now have transform matrix, with orientation & camera location
+        let cameraTransformer = centerPoint.transform
+        let cameraLocation = SCNVector3(cameraTransformer.m41,cameraTransformer.m42,cameraTransformer.m43)
+        let cameraOrientation = SCNVector3(-cameraTransformer.m31,-cameraTransformer.m32,-cameraTransformer.m33)
+        let cameraPosition = SCNVector3Make(cameraLocation.x + cameraOrientation.x, cameraLocation.y + cameraOrientation.y, cameraLocation.z + cameraOrientation.z)
+        // x1 + x2, y1 + y2, z1 + z2 ^
+        let box = SCNSphere(radius: 8.15)
+        let natural = SCNMaterial()
+        natural.diffuse.contents = UIImage(named: "texture.png")
+        //diffuse (how light renders), contents (appearance of material)
+        box.materials = [natural]
+        let boxNode = SCNNode(geometry: box)
+        //must be node^
+        boxNode.position = cameraPosition
+        let physicsShape = SCNPhysicsShape(node: boxNode, options: nil)
+        let physicsBody = SCNPhysicsBody(type: .dynamic, shape: physicsShape)
+        boxNode.physicsBody = physicsBody
+        boxNode.physicsBody?.applyForce(SCNVector3(cameraPosition.x,cameraPosition.y,cameraPosition.z), asImpulse: true)
+        sceneView.scene.rootNode.addChildNode(boxNode)
     }
     // MARK: - ARSCNViewDelegate
     
