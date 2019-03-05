48070540e3c8616a09c8fe20ecda1288dbd980e3
jaebooker <jaesonbooker@gmail.com>
2019-03-04 22:58:25 -0800

    created API struct

---
+++ ARcache/Cache.swift
@@ -0,0 +1,15 @@
+//
+//  Cache.swift
+//  ARcache
+//
+//  Created by Jaeson Booker on 3/4/19.
+//  Copyright © 2019 Jaeson Booker. All rights reserved.
+//
+
+import Foundation
+
+struct Swift: Encodable, Decodable {
+    var notes: [String]
+    let xcoordinate: Double
+    let ycoordinate: Double
+}
